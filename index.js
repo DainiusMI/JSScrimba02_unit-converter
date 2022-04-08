9a9161d639acdd3ef76a1e1a14a571ade775c68d
Dainius <100964434+DainiusMI@users.noreply.github.com>
2022-04-08 20:16:03 +0300

    themes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 
-
-
-
 function convert() {
     let inputedValue = document.getElementById('inputed-number');
+
     let metersToFeet = inputedValue.value * 3.28084;
     let feetToMeters = inputedValue.value / 3.28084;
+    
     let litersToGallons = inputedValue.value * 0.264172;
     let gallonsToLiters = inputedValue.value / 0.264172;
+    
     let kilogramsToPounds = inputedValue.value * 2.2046;
     let poundsToKilograms = inputedValue.value / 2.2046;
 
@@ -20,7 +20,12 @@ function convert() {
     
     document.getElementById('kilogram-pound').textContent = inputedValue.value + " Kilograms = "  + kilogramsToPounds.toFixed(3) + " Pounds";
     document.getElementById('pound-kilogram').textContent = inputedValue.value + " Pounds = "  + poundsToKilograms.toFixed(3) + " Kilograms";
-
+}
+function changeTheme() {
+    let bottomSectionEl = document.getElementById('bottom-section');
+    let themeButtonEl = document.getElementById('theme-button');
+    bottomSectionEl.classList.toggle('dark-theme');
+    themeButtonEl.classList.toggle('light-theme');
 
 
 }
