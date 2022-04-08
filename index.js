b932356bc2a6b667182c283b6b5b0b073923e657
Dainius <100964434+DainiusMI@users.noreply.github.com>
2022-04-08 19:13:53 +0300

    ver 1

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+function convert() {
+    let inputedValue = document.getElementById('inputed-number');
+    let metersToFeet = inputedValue.value * 3.28084;
+    let feetToMeters = inputedValue.value / 3.28084;
+    let litersToGallons = inputedValue.value * 0.264172;
+    let gallonsToLiters = inputedValue.value / 0.264172;
+    let kilogramsToPounds = inputedValue.value * 2.2046;
+    let poundsToKilograms = inputedValue.value / 2.2046;
+
+    document.getElementById('meter-feet').textContent = inputedValue.value + " meters = "  + metersToFeet.toFixed(3) + " feet";
+    document.getElementById('feet-meter').textContent = inputedValue.value + " feet = "  + feetToMeters.toFixed(3) + " meters";
+
+    document.getElementById('liter-gallon').textContent = inputedValue.value + " Liters = "  + litersToGallons.toFixed(3) + " Gallons";
+    document.getElementById('gallon-liter').textContent = inputedValue.value + " Gallons = "  + gallonsToLiters.toFixed(3) + " Liters";
+    
+    document.getElementById('kilogram-pound').textContent = inputedValue.value + " Kilograms = "  + kilogramsToPounds.toFixed(3) + " Pounds";
+    document.getElementById('pound-kilogram').textContent = inputedValue.value + " Pounds = "  + poundsToKilograms.toFixed(3) + " Kilograms";
+
+
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# UNIT COVERTER
+This is a solo project from a free javascript course provided by scrimba.com
+## FIGMA DESIGN LINK: 
+https://www.figma.com/file/AdqUVRQCZGP1zRcEvzmJTm/Unit-Conversion?node-id=0%3A1
